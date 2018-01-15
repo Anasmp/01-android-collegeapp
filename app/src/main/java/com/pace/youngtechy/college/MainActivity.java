9578ca3fd32556e9d6065566c3f26da9a33153ee
youngtechydotcom <youngtechy.com@gmail.com>
2018-01-15 08:47:38 +0530

    Initial commit

---
+++ app/src/main/java/com/pace/youngtechy/college/MainActivity.java
@@ -0,0 +1,25 @@
+package com.pace.youngtechy.college;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // TODO:6 carview references
+
+        // TODO:7 setonclicklistner for cardview 1 and add setonclicklistner for cardview 2
+
+        // TODO:8 call activity onclicklistner
+
+        // TODO:10 web activity
+
+    }
+
+    // TODO:9 method for call activity
+}
